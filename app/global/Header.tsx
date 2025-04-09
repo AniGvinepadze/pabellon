0031cfee27450f95310c2bbe8c323013f043a9ac
Ani <a.gvin3@gmail.com>
2025-04-09 16:26:15 +0400

    feat(home):global

--- app/global/Header.tsx
+++ app/global/Header.tsx
@@ -1,28 +1,70 @@
-import Image from 'next/image'
-import React from 'react'
-import logo from "../../public/assets/logo.svg"
-import { navbar } from '..'
-import glovabIcon from "../../public/assets/image 2 (2).svg"
-import Link from 'next/link'
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+import logo from "../../public/assets/logo.svg";
+import { navbar } from "..";
+import glovabIcon from "../../public/assets/image 2 (2).svg";
+import Link from "next/link";
+import mobilemenu from "../../public/assets/Vector.svg";
+import { div } from "framer-motion/client";
+import MobileMenu from "./MobileMenu";
+
 export default function Header() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [popup,setPopup] = useState<boolean>(false)
+
   return (
-    <div className='max-w-[1600px] w-full m-auto p-3 flex justify-between items-center'>
-     <Link href="/home" className='hover:scale-110 transition-all ease-in-out duration-300'>
-     <Image src={logo} alt='logo' width={300} height={90} className='max-w-[300px] w-full object-contain'/>
-     </Link>
-     <div className='flex gap-6 mt-1 max-1000:hidden'>
-        <div className='flex gap-7 items-center mt-4' >
-           {navbar.map((e)=>(
-            <Link href={e.route} key={e.id} className='max-w-[700px] w-full hover:scale-110 transition-all ease-in-out duration-300'>
-              <p className='text-xl font-medium'>{e.title} </p>
+    <div className="max-w-[1600px] w-full m-auto p-5 flex justify-between items-center max-1000:gap-10">
+      <Link
+        href="/home"
+        className="hover:scale-110 transition-all ease-in-out duration-300"
+      >
+        <Image
+          src={logo}
+          alt="logo"
+          width={300}
+          height={90}
+          className="max-w-[300px] w-full object-contain"
+        />
+      </Link>
+      <div className="flex gap-6 mt-1 max-1000:hidden">
+        <div className="flex gap-7 items-center mt-4">
+          {navbar.map((e) => (
+            <Link
+              href={e.route}
+              key={e.id}
+              className="max-w-[700px] w-full hover:scale-110 transition-all ease-in-out duration-300"
+            >
+              <p className="text-xl font-medium">{e.title} </p>
             </Link>
-           ))}
+          ))}
         </div>
-        <div className='hover:scale-110 transition-all ease-in-out duration-300 mt-4'>
-            <Image src={glovabIcon} alt='globalIcon ' width={43} height={43} className="cursor-pointer"/>
+        <div className="relative ">
+        <button className="hover:scale-110 transition-all ease-in-out duration-300 mt-4" onClick={() => setPopup((prev) => !prev)}>
+          <Image
+            src={glovabIcon}
+            alt="globalIcon "
+            width={43}
+            height={43}
+            className="cursor-pointer mt-2"
+          />
+      
+        </button>
+        {popup &&(
+            <div className="absolute w-[150px]  items-center -right-2  p-4 shadow-md rounded-md"> 
+                <p className="text-base font-medium my-2 cursor-pointer hover:text-lg transition-all ease-in-out duration-300">English</p>
+                <p className="text-base font-medium my-2 cursor-pointer hover:text-lg transition-all ease-in-out duration-300">Georgia</p>
+            </div>
+        )}
         </div>
-     </div>
-     
+      </div>
+      <button
+        className="hidden max-1000:flex hover:scale-110 mt-3 transition-all ease-in-out duration-300"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <Image src={mobilemenu} alt="mobilemenu" width={35} height={35} />
+      </button>
+      {isOpen && <MobileMenu isOpen={isOpen} setIsOpen={setIsOpen} popup={popup} setPopup={setPopup} />}
     </div>
-  )
+  );
 }
---
+++ app/global/MobileMenu.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import globalIcon from "../../public/assets/image 2 (2).svg";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { navbar } from "..";
+
+interface MobileMenuProps {
+  isOpen: boolean;
+  setIsOpen: (value: boolean) => void;
+  popup: boolean;
+  setPopup: (value: boolean) => void;
+}
+
+export default function MobileMenu({
+  isOpen,
+  setIsOpen,
+  popup,
+  setPopup,
+}: MobileMenuProps) {
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          className="fixed top-20 left-0 right-0 z-50  max-1000:block hidden"
+          initial={{ y: -40, opacity: 0 }}
+          animate={{ y: 0, opacity: 2 }}
+          exit={{ y: -40, opacity: 0 }}
+          transition={{ duration: 0.8, ease: "easeInOut" }}
+        >
+          <div className="w-full shadow-md  p-4 max-350:p-2">
+            <div className="flex flex-col justify-between h-full text-black">
+              <div className="flex flex-col gap-5 px-7 py-3 max-400:gap-2">
+                {navbar.map((item) => (
+                  <Link
+                    key={item.id}
+                    href={item.route}
+                    onClick={() => setIsOpen(false)}
+                  >
+                    <p className="text-lg hover:text-[19px] font-medium transition-all duration-300  ease-in-out max-300:text-base">
+                      {item.title}
+                    </p>
+                    <div className="hover:scale-110 transition-all ease-in-out duration-300 mt-4">
+                      {" "}
+                    </div>
+                  </Link>
+                ))}
+
+                <div className="relative w-fit -ml-4">
+                  <button onClick={() => setPopup((prev) => !prev)}>
+                    <Image
+                      src={globalIcon}
+                      alt="globalIcon"
+                      width={43}
+                      height={43}
+                      className="cursor-pointer hover:w-[50px] transition-all ease-in-out duration-300"
+                    />
+                  </button>
+                  {popup && (
+                    <div className="absolute top-12 left-0 bg-white w-[150px] p-4 shadow-md rounded-md z-50">
+                      <p className="text-base font-medium my-2 cursor-pointer hover:text-lg transition-all ease-in-out duration-300">
+                        English
+                      </p>
+                      <p className="text-base font-medium my-2 cursor-pointer hover:text-lg transition-all ease-in-out duration-300">
+                        Georgia
+                      </p>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
