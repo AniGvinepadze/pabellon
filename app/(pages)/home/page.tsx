f0d68083beaea84198e7d06be869fe37c9524f7c
Ani <a.gvin3@gmail.com>
2025-04-09 17:11:49 +0400

    feat(header):sticky

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='max-w-[1600px] w-full m-auto p-10'></div>
+    <div className='max-w-[1600px]  h-[2000px] w-full m-auto p-10'></div>
   )
 }
