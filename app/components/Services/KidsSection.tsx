0e67e2a2c1cfb38db64f751b7b4a8f9530422318
Ani <a.gvin3@gmail.com>
2025-06-12 21:21:12 +0400

    fix kids Section

--- app/components/Services/KidsSection.tsx
+++ app/components/Services/KidsSection.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 export default function KidstSection() {
   return (
-    <div className="max-w-[1400px] w-full m-auto flex flex-col justify-center items-center text-center text-secondaryTextColor">
-      <div className="max-w-[1000px] w-full flex flex-col justify-center items-center text-center text-secondaryTextColor font-mono-serif gap-6 max-400:gap-3">
+    <div className="max-w-[1400px] w-full m-auto flex flex-col justify-center items-center text-center text-secondaryTextColor my-20">
+      <div className="max-w-[1000px] w-full flex flex-col justify-center items-center text-center text-secondaryTextColor font-mono-serif gap-6 max-400:gap-3 " >
         <h2 className="font-semibold text-[40px] italic  max-600:text-[32px] max-400:text-[26px]">
           Kids Entertainment
         </h2>
@@ -29,10 +29,10 @@ export default function KidstSection() {
           className="max-w-[1600px] w-full "
         />
       </div>
-      <div className="grid grid-cols-4 gap-5 w-full">
+      <div className="grid grid-cols-4 gap-5 w-full my-8">
         <Image src={kidsErImg2} alt="icon" width={388} height={419} className="mt-7"/>
         <Image src={kidsErImg3} alt="icon" width={388} height={508} />{" "}
-        <Image src={kidsErImg4} alt="icon" width={385} height={419}  className="object-contain max-h-[400px] mt-3"/>{" "}
+        <Image src={kidsErImg4} alt="icon" width={322} height={419}  className="object-contain max-h-[400px] mt-3"/>{" "}
         <Image src={kidsErImg5} alt="icon" width={388} height={508} />
       </div>
     </div>
