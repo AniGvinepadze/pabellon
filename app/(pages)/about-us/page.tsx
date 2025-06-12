16a0dfef08cb660aac4bca4106654b33512c01f0
Ani <a.gvin3@gmail.com>
2025-06-12 15:10:46 +0400

    feat ( aboutUssection responsive )

--- app/(pages)/about-us/page.tsx
+++ app/(pages)/about-us/page.tsx
@@ -4,7 +4,9 @@ import React from 'react'
 export default function AboutUs() {
   return (
     <div className='max-w-[1600px] m-auto p-3'>
+        <div className='mt-28'>
         <AboutUsaSection/>
+        </div>
     </div>
   )
 }
--- app/components/About-us/AboutUsaSection.tsx
+++ app/components/About-us/AboutUsaSection.tsx
@@ -5,12 +5,12 @@ import React from "react";
 export default function AboutUsaSection() {
   return (
     <div className="max-w-[1400px] w-full m-auto flex flex-col justify-center items-center text-center text-secondaryTextColor">
-      <div className="w-full flex flex-col justify-center items-center text-center text-secondaryTextColor font-mono-serif gap-6">
-        <h2 className="font-semibold text-[40px] italic  ">About Us</h2>
-        <h2 className="font-normal text-[30px] italic">
+      <div className="w-full flex flex-col justify-center items-center text-center text-secondaryTextColor font-mono-serif gap-6 max-400:gap-3">
+        <h2 className="font-semibold text-[40px] italic  max-600:text-[32px] max-400:text-[26px]">About Us</h2>
+        <h2 className="font-normal text-[30px] italic max-600:text-[22px] max-400:text-[18px]">
          inspired by traditional Georgian archeology, art, literature, history and motifs // შესაცვლელია
         </h2>
-        <p className="text-[17px] font-light max-w-[780px]">
+        <p className="text-[17px] font-light max-w-[780px] max-600:text-[15px] max-400:text-[13px]">
         We promise an escape to a world of warmth, generosity, and playful delight, where every detail is designed to spark joy and create lasting memories. Our mission is to create a haven of hospitality that ignites childlike wonder and unforgettable memories for our guests. 
         </p>
       </div>
