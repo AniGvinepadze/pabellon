110446f4673ce8f61d0fe5116f24b2854165b446
Ani <a.gvin3@gmail.com>
2025-06-15 18:13:44 +0400

    fix spacesSectuin

--- app/components/Meetings-events/SpacesSection.tsx
+++ app/components/Meetings-events/SpacesSection.tsx
@@ -9,17 +9,17 @@ export default function SpacesSection() {
           Spaces
         </h2>
       </div>
-      <div className="max-w-[850px] bg-secondaryTextColor w-full my-20">
-      <div className=" grid grid-cols-3  max-w-[850px] w-full max-750:grid-cols-1 max-500:my-10">
+      <div className="max-w-[850px] bg-secondaryTextColor w-full my-20 p-7 max-750:p-2">
+      <div className=" grid grid-cols-3  max-w-[850px] w-full max-750:grid-cols-1 mb-8">
         {sapces.map((e) => (
-          <div key={e.id} className="bg-secondaryTextColor w-full p-6 max-750:p-2">
+          <div key={e.id} className="bg-secondaryTextColor w-full  max-750:p-2">
             <li className="font-light text-xl text-beige max-800:text-lg">{e.title}</li>
           </div>
         ))}
       </div>
        <div className=" grid grid-cols-2  max-w-[850px] w-full max-750:grid-cols-1 max-500:my-10">
         {sapces2.map((e) => (
-          <div key={e.id} className="bg-secondaryTextColor w-full p-6 max-750:p-2">
+          <div key={e.id} className="bg-secondaryTextColor w-full ">
             <li className="font-light text-xl text-beige max-800:text-lg">{e.title}</li>
           </div>
         ))}
