84c20ca1c2beb7bc95e0298a83c38a5a0ee28354
Ani <a.gvin3@gmail.com>
2025-06-15 19:20:26 +0400

    update

--- app/components/Meetings-events/ConferenceRoomsSection.tsx
+++ app/components/Meetings-events/ConferenceRoomsSection.tsx
@@ -1,5 +1,11 @@
-import { artWork, meetingImg } from "@/app/assets";
+"use client"
+import { artWork, meetingImg, meetingImg1 } from "@/app/assets";
 import Image from "next/image";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
 import React from "react";
 
 export default function ConferenceRoomsSection() {
@@ -9,7 +15,7 @@ export default function ConferenceRoomsSection() {
         <h2 className="font-semibold text-[40px] italic  max-600:text-[32px] max-400:text-[26px]">
           Conference Rooms
         </h2>
-        <p className="font-normal text-[17px] max-600:text-[15px] max-400:text-[13px]">
+        <p className="font-light text-[17px] max-600:text-[15px] max-400:text-[13px]">
           Our fully equipped conference rooms offer a modern, flexible
           environment for business gatherings. Whether you’re hosting a
           high-level corporate meeting, a training seminar, or a grand
@@ -25,8 +31,34 @@ export default function ConferenceRoomsSection() {
       </div>
 
       <div className=" my-20 flex flex-col gap-14 justify-center items-center">
-        <Image src={meetingImg} alt="meetingImg" width={1420} height={877} />
+        <Image src={meetingImg1} alt="meetingImg" width={1420} height={877} />
       </div>
+       <div className="w-full max-w-4xl mx-auto mt-10">
+      <Swiper
+        modules={[Autoplay, Pagination]}
+        spaceBetween={20}
+        slidesPerView={4}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        loop={true}
+      >
+        <SwiperSlide>
+          <div className="h-64 bg-blue-500 text-white flex items-center justify-center text-2xl">
+            Slide 1
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="h-64 bg-green-500 text-white flex items-center justify-center text-2xl">
+            Slide 2
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="h-64 bg-red-500 text-white flex items-center justify-center text-2xl">
+            Slide 3
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
     </div>
   );
 }
