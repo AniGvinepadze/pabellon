1466830f82ac5417145251f11990404eb39565b9
Ani <a.gvin3@gmail.com>
2025-06-17 13:24:37 +0400

    feat add wellnessSection

---
+++ app/(pages)/wellness-fitness/page.tsx
@@ -0,0 +1,10 @@
+import WellnessSection from "@/app/components/Services/WellnessSection";
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="max-w-[1600px] m-auto p-3">
+      <WellnessSection />
+    </div>
+  );
+}
