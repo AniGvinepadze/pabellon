3031e28d7fc18ac5800ae2f2623d60267ea18e1e
Ani <a.gvin3@gmail.com>
2025-06-17 13:25:27 +0400

    update welnness Section

--- app/(pages)/wellness-fitness/page.tsx
+++ app/(pages)/wellness-fitness/page.tsx
@@ -4,7 +4,9 @@ import React from "react";
 export default function page() {
   return (
     <div className="max-w-[1600px] m-auto p-3">
+        <div className="my-20">
       <WellnessSection />
+      </div>
     </div>
   );
 }
