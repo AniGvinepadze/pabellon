1775a0abaa42fe08aa2468046a5b5022d7320531
Ani <a.gvin3@gmail.com>
2025-06-19 16:48:31 +0400

    Feat ( Makrina-restaurant )   : feat makrina further question section

--- app/(pages)/makrine-restaurant/page.tsx
+++ app/(pages)/makrine-restaurant/page.tsx
@@ -1,9 +1,11 @@
 import MakrineBarSection from '@/app/components/Makrine-Resraurant/MakrineBarSection'
 import MakrineLobbyBartSection from '@/app/components/Makrine-Resraurant/MakrineLobbyBarSection'
+import MakrinePoolBarSection from '@/app/components/Makrine-Resraurant/MakrinePoolbarsection'
 import MakrineRestaurantBookSection from '@/app/components/Makrine-Resraurant/MakrineRestaurantBookSection'
 import MakrinerestaurantSection from '@/app/components/Makrine-Resraurant/MakrineRestaurantSection'
 import MakrineSection from '@/app/components/Makrine-Resraurant/MakrineSection'
 import MakrineSwiperSection from '@/app/components/Makrine-Resraurant/MakrineSwiperSection'
+import FurtherQuestionSection from '@/app/components/Meetings-events/FurtherQuestionsSection'
 import React from 'react'
 
 export default function page() {
@@ -17,6 +19,8 @@ export default function page() {
         <MakrinerestaurantSection/>
         <MakrineBarSection/>
         <MakrineLobbyBartSection/>
+        <MakrinePoolBarSection/>
+        <FurtherQuestionSection/>
         </div>
         </div>
         </div>
