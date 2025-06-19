53d4172c59404322e723eb397a24e5db583aa04d
Ani <a.gvin3@gmail.com>
2025-06-19 12:55:48 +0400

    Feat ( Rooms ) : Feat roomssection responsive

--- app/components/Rooms/RoomsToBookSection.tsx
+++ app/components/Rooms/RoomsToBookSection.tsx
@@ -6,23 +6,23 @@ export default function RoomsToBookSection() {
   return (
     <div>
       {" "}
-      <div className="w-full grid grid-cols-2 gap-10 my-16 max-550:grid-cols-1">
+      <div className="w-full grid grid-cols-2 gap-10 my-16 max-750:grid-cols-1 max-750:gap-0">
         {roomsBook.map((e) => (
           <div
             key={e.id}
-            className="max-w-[714px] w-full  flex flex-col  gap-5 max-1100:my-10 "
+            className="max-w-[714px] w-full  flex flex-col  gap-5 max-1100:my-10 max-750:my-4 "
           >
             <div className="">
               <Image src={e.img} alt={e.title} width={714} height={429} />
             </div>
 
-            <div className="flex flex-col  text-start gap-8  max-w-[550px] w-full ">
-              <h2 className="font-semibold italic text-[25px]">{e.title}</h2>
-              <p className="font-normal text-[15px]">{e.description}</p>
+            <div className="flex flex-col  text-start gap-8  max-w-[550px] w-full max-500:gap-4 ">
+              <h2 className="font-semibold italic text-[25px] max-500:text-xl">{e.title}</h2>
+              <p className="font-normal text-[15px] ">{e.description}</p>
             </div>
 
             <div className="flex flex-col mt-auto ">
-               <div className="flex justify-between">
+               <div className="flex justify-between max-1200:flex-col ">
               <div className="">
                 <div className="w-full flex gap-[14px] max-1350:flex-wrap my-4 ">
                   <div className="flex gap-1">
@@ -71,7 +71,7 @@ export default function RoomsToBookSection() {
                 </div>
               </div>
               <div>
-                <p className="font-normal text-[40px]">${e.price}<span className="text-xs font-normal mx-2">From / Per Night</span></p>
+                <p className="font-normal text-[40px] max-500:text-[28px]">${e.price}<span className="text-xs font-normal mx-2">From / Per Night</span></p>
               </div>
             </div>
             </div>
