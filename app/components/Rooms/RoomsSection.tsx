89b02c1b0e458f12d16d88125e3dbe868016a471
Ani <a.gvin3@gmail.com>
2025-06-19 12:57:26 +0400

    Fix ( Rooms ) : fix rooms section responsive

--- app/components/Rooms/RoomsSection.tsx
+++ app/components/Rooms/RoomsSection.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function RoomsSection() {
   return (
-    <div className="max-w-[1400px] w-full m-auto flex flex-col justify-center items-center text-center text-secondaryTextColor my-20">
+    <div className="max-w-[1400px] w-full m-auto flex flex-col justify-center items-center text-center text-secondaryTextColor my-20 ">
       <div className="max-w-[750px] w-full flex flex-col justify-center items-center text-center text-secondaryTextColor font-mono-serif gap-6 max-400:gap-3">
         <h2 className="font-semibold text-[40px] italic  max-600:text-[32px] max-400:text-[26px]">
  Rooms
@@ -18,7 +18,7 @@ export default function RoomsSection() {
         </p>
       </div>
 
-      <div className=" my-20 flex flex-col gap-14 justify-center items-center">
+      <div className=" mt-20 flex flex-col gap-14 justify-center items-center ">
         <Image src={roomImg} alt="meetingImg" width={1420} height={877}/>
       </div>
     </div>
