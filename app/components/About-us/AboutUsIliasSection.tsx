f343a750aac062520103b33bcb53c94cefc219d3
Ani <a.gvin3@gmail.com>
2025-06-26 14:00:51 +0400

    Fix  ( About-us ) : update text content

--- app/components/About-us/AboutUsIliasSection.tsx
+++ app/components/About-us/AboutUsIliasSection.tsx
@@ -6,18 +6,27 @@ export default function AboutUsIliasSection() {
   return (
     <div className="max-w-[1300px] w-full m-auto flex flex-col justify-center items-center text-center text-secondaryTextColor ">
       <div className=" max-w-[1000px] w-full flex flex-col justify-center items-center text-center text-secondaryTextColor font-mono-serif gap-16">
-        <p className="font-light text-[17px] max-500:text-[15px]">
-          Modern and elegantly designed rooms, stunning views of Alazani Valley,
-          authentic dining options, gastronomic adventures and masterclasses,
-          indoor and outdoor pool, spa and fitness, kids entertainment,
-          conference rooms, personalized guest services to cater to your every
-          need through the events. And of course, the Wine! We invite you to
-          explore and enjoy the excellence of homegrown craftsmanship in every
-          glass. Savor the unique character of our domestic wines, crafted with
-          care from vineyards right here at home. From bold reds to crisp
-          whites, each bottle tells a story of local passion, sustainable
-          practices and winemaking traditions.
-        </p>
+        <div className="flex flex-col gap-6">
+          <p className="font-light text-[17px] max-500:text-[15px]">
+            At “Pabeloni,” inspired by Georgian hospitality, art, literature,
+            and history, you'll discover a world where warmth, generosity, and
+            joy-filled adventure await you. Here, every detail is connected to
+            history, adding nostalgic notes to your unforgettable memories and
+            touching an especially dear past.
+          </p>
+          <p className="font-light text-[17px] max-500:text-[15px]">
+            Modern and elegant rooms with stunning views over the Alazani
+            Valley, authentic culinary masterpieces, gastronomic adventures and
+            masterclasses, indoor and outdoor pools, spa and fitness, children's
+            play areas, conference halls, and personalized service — everything
+            is designed for your comfort and for holding events perfectly.
+          </p>
+          <p className="font-light text-[17px] max-500:text-[15px]">
+            And of course — wine! Taste our variety of unique wines, each made
+            from grapes grown in Kakheti's vineyards. Every bottle tells a story
+            about the traditions of local winemaking.
+          </p>
+        </div>
         <Image
           priority={true}
           src={iliaLake}
@@ -29,18 +38,9 @@ export default function AboutUsIliasSection() {
       <div className="flex flex-col gap-24 justify-center mt-16 max-450:gap-12">
         <p className="font-light text-[17px] max-500:text-[15px]">
           In Kvareli, where Ilia Chavchavadze spent his childhood, the
-          surroundings create an atmosphere WHERE STORIES LIVE. While standing
-          under the majestic canopy of a walnut tree, you feel the timeless
-          moment that reflects the essence of our brand and its connection to
-          Georgian heritage and literature. And Makrine, Aunt - who raised him
-          as her own child, is someone who values family, tradition and sharing
-          a delicious meal in a beautiful setting. We promise a warm and
-          nurturing dining experience, inspired by Makrine's legacy, where you
-          can taste delicious food and create lasting memories with loved ones.
-          Farm to table movement highlights the importance of nutritional value
-          and community engagement. In addition of the true agricultural
-          concept, it is a luxurious destination where comfort, style and
-          unforgettable experiences meet.
+          surroundings create a fitting atmosphere and bring history to life. It
+          is precisely under the shade of the magnificent walnut tree that the
+          heritage of Georgia is felt, taking us back to Ilia’s era.
         </p>
         <div className="flex justify-center">
           <Image
@@ -51,16 +51,24 @@ export default function AboutUsIliasSection() {
             height={178}
           />
         </div>
-        <p className="font-light text-[17px] max-500:text-[15px]">
-          And Makrine, Aunt - who raised him as her own child, is someone who
-          values family, tradition and sharing a delicious meal in a beautiful
-          setting. We promise a warm and nurturing dining experience, inspired
-          by Makrine's legacy, where you can taste delicious food and create
-          lasting memories with loved ones. Farm to table movement highlights
-          the importance of nutritional value and community engagement. In
-          addition of the true agricultural concept, it is a luxurious
-          destination where comfort, style and unforgettable experiences meet.
-        </p>
+        <div className="flex flex-col gap-6">
+          <p className="font-light text-[17px] max-500:text-[15px]">
+            Makrine — the aunt who raised Ilia as her son — always valued
+            family, tradition, and the true culture of Georgian hospitality.
+            That's why the table created in her memory is a place where you can
+            enjoy delicious dishes and create unforgettable memories with your
+            loved ones.
+          </p>
+          <p className="font-light text-[17px] max-500:text-[15px]">
+            <p className="font-light text-[17px] max-500:text-[15px]">
+              The “farm-to-table” concept highlights the value of each dish and
+              the importance of local community involvement. But beyond its
+              authentic agricultural roots, this is a truly Georgian tradition —
+              a direction where comfort, style, and unforgettable experiences
+              come together.
+            </p>
+          </p>
+        </div>
       </div>
     </div>
   );
