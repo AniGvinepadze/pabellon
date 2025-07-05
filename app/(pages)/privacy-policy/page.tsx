d265b52a28e807fa48549fcd223a40a7cfc6de8b
Ani <a.gvin3@gmail.com>
2025-07-05 20:33:17 +0400

    Feat (Privacy-policy) : add privacy-policy page

---
+++ app/(pages)/privacy-policy/page.tsx
@@ -0,0 +1,206 @@
+import { logoCol } from "@/app/assets";
+import Image from "next/image";
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="max-w-[1440px] w-full m-auto">
+      <div className="max-w-[1200px] w-full m-auto p-4 my-20">
+        <div className="w-full flex flex-col justify-center items-center gap-3">
+          <Image src={logoCol} alt="logo" width={200} height={80} />
+          <p className="font-bold text-lg mt-5">Privacy Statement</p>
+          <p className="font-bold text-base text-[#646464]">Pabellón Hotel</p>
+        </div>
+        <div className="w-full flex justify-between my-6">
+          <div></div>
+          <p className="font-semibold text-sm italic">Created: May, 2025</p>
+        </div>
+        <div className="w-full flex flex-col gap-10 text-[#949292]">
+          <div>
+            <p className="font-semibold text-lg text-[#646464]">
+              At Pabellón Hotel are committed to protecting your privacy, and
+              support a general policy of openness about how we collect, use and
+              disclose your personal information.
+            </p>
+          </div>
+          <div>
+            <p className="font-semibold text-xl text-[#646464]">
+              OUR COMMITMENT
+            </p>
+            <p className="text-medium text-lg">
+              As our valued guest (“You”), we value your commitment to Pabellón
+              Hotel. Our first priority is to offer You exceptional stays at our
+              hotels and we recognize that your privacy forms part of our
+              commitment to our guests. Our Guest Privacy Policy is outlined
+              below which describes how Pabellón Hotel use your personal
+              information.
+            </p>
+          </div>
+          <div>
+            <p className="font-semibold text-xl text-[#646464]">
+              CONSENT TO THIS HOTEL GUEST PRIVACY POLICY
+            </p>
+            <p className="text-medium text-lg">
+              Please read our Guest Privacy Policy carefully before providing us
+              with any of your Personal Information (“PI”), i.e. any information
+              collected and recorded in any format that identifies You
+              personally, whether directly (e.g. name) or indirectly (e.g. phone
+              number). This Guest Privacy Policy is part of Pabellón Hotel's
+              terms and conditions governing our hotel services. By accepting
+              said terms and conditions, You expressly consent to this Guest
+              Privacy Policy.
+            </p>
+          </div>
+          <div>
+            <p className="font-semibold text-xl text-[#646464]">
+              WHO COLLECTS MY DATA?
+            </p>
+            <p className="text-medium text-lg">
+              The hotel operates as part of the hotel management group, Hotel
+              Collection International. Your data will be collected by the
+              hotel. The data is collected during the booking process either
+              through the hotel's reservation system or through an independent
+              third-party reservation system provider.
+            </p>
+          </div>
+          <div>
+            <p className="font-semibold text-xl text-[#646464]">
+              WHEN IS MY PERSONAL INFORMATION COLLECTED?
+            </p>
+            <p className="text-medium text-lg">
+              We collect information from you when you make a booking through
+              our reservation system. Information collected during the course of
+              the reservation and during your stay may include:
+            </p>
+            <p className="text-medium text-lg pl-16">
+              Your name, email address, home and business address, phone number,
+              nationality and payment card information. Information such as stay
+              and room preferences made during the course of your reservation
+              such as your preferred room type and specific requests to the
+              hotel.
+            </p>
+            <p className="text-medium text-lg mt-5">
+              This information may be provided to us directly when you make a
+              reservation through our websites, or directly at the hotel. In
+              some cases, we will receive this information from a third-party,
+              such as when you book through an online travel agency.
+            </p>
+          </div>
+          <div>
+            <p className="font-semibold text-xl text-black">
+              How we use information:
+            </p>
+            <p className="text-medium text-lg">
+              We use the information collected from you primarily to fulfill
+              your hotel reservation. Following your stay, we may also send you
+              post-stay communications and satisfaction surveys to get feedback
+              on your experience.
+            </p>
+          </div>
+          <div>
+            <p className="font-semibold text-xl text-black">
+              Who we share your information with:
+            </p>
+            <p className="text-medium text-lg">
+              We may share your information with third-party service providers
+              to provide services in relation to our business as well as to help
+              us improve our products and services. For example, we might use a
+              third party or a third-party product for the purposes of market
+              research and data analytics. We also share your stay information
+              with other third parties in circumstances such as: When we believe
+              in good faith that the disclosure is required by law or to protect
+              the safety of hotel guests, employees; When disclosure is required
+              to comply with a judicial proceeding, court order, subpoena,
+              warrant or legal process; In the event of a merger, asset sale or
+              other related transaction.
+            </p>
+          </div>
+          <div>
+            <p className="font-semibold text-xl text-black">
+              The legal basis for processing your personal data
+            </p>
+            <p className="text-medium text-lg">
+              We are committed to collecting and using your information in
+              accordance with applicable data protection laws. We will only
+              collect, use and share your information where we are satisfied
+              that we have an appropriate legal basis to do this. This may be
+              because: You have provided your consent to us using the personal
+              information; Our use of your information is necessary to perform
+              our contract with you, for example, making and managing your
+              booking; Our use of your information is necessary to meet
+              responsibilities we have to our regulators, tax officials, law
+              enforcement, or otherwise meet our legal responsibilities; Our use
+              of your information is in our legitimate interest as a commercial
+              organisation, for example to operate and improve our services and
+              to keep people informed about our products and services (including
+              for profiling and targeted advertising) - in these cases we will
+              look after your information at all times in a way that is
+              proportionate and respects your privacy rights and you have a
+              right to object to processing as explained in.
+            </p>
+          </div>
+          <div>
+            <p className="font-semibold text-xl text-black">
+              How we secure your information
+            </p>
+            <p className="text-medium text-lg">
+              We are committed to protecting the confidentiality and security of
+              the information that you provide to us. To do this, technical,
+              physical and organizational security measures are put in place to
+              protect against any unauthorized access, disclosure, damage or
+              loss of your information. The collection, transmission and storage
+              of information can never be guaranteed to be completely secure,
+              however, we take steps to ensure that appropriate security
+              safeguards are in place to protect your information.
+            </p>
+          </div>
+          <div>
+            <p className="font-semibold text-xl text-black">Children</p>
+            <p className="text-medium text-lg">
+              Our website is not intended for children and we do not
+              intentionally solicit or collect personal information from
+              individuals under the age of 18. If we are notified or otherwise
+              discover that a minor’s personal information has been improperly
+              collected, we will take all commercially reasonable steps to
+              delete that information. In limited instances, we may have a
+              campaign or program targeted toward children. In these instances,
+              details on the information practices will be presented within the
+              terms and conditions of the program or campaign.
+            </p>
+          </div>
+          <div>
+            <p className="font-semibold text-xl text-black">
+              Retaining your information in our systems
+            </p>
+            <p className="text-medium text-lg">
+              We generally only keep your information for as long as is
+              reasonably required for the reasons explained in this privacy
+              policy. In some cases we keep transactional records (which may
+              include your information) for longer periods if necessary to meet
+              legal, regulatory, tax or accounting needs. We will also retain
+              information if we reasonably believe there is a prospect of
+              litigation.
+            </p>
+          </div>
+          <div className="flex flex-col gap-[2px]" >
+            <p className="font-semibold text-xl text-black">
+              How to contact us
+            </p>
+            <p className="font-medium text-base ">
+              For any questions or concerns regarding this Privacy Statement or
+              our data privacy practices,
+            </p>
+            <p className="text-medium text-base">please contact us:</p>
+
+            <p className="font-medium text-base">By email: info@pabellon.ge</p>
+            <p className="font-medium text-base">LTD “Wine Road 2017”</p>
+            <p className="font-medium text-base">Kvareli, Ilia Lake, Georgia</p>
+            <p className="font-medium text-base">
+              By phone: (+995) 35 2 200 200
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
