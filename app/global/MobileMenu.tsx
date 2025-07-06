7e560820f4a02a854a36052f90bf7e0b90dfba21
Ani <a.gvin3@gmail.com>
2025-07-06 14:46:09 +0400

    Fix (Mobile-menu ) : fixed final bugs

--- app/global/MobileMenu.tsx
+++ app/global/MobileMenu.tsx
@@ -5,6 +5,7 @@ import whiteglovabIcon from "../../public/assets/Vector (9).svg";
 import glovabIcon from "../../public/assets/Vector (10).svg";
 import { AnimatePresence, motion } from "framer-motion";
 import { navbar } from "..";
+import { set } from "date-fns";
 
 interface MobileMenuProps {
   isOpen: boolean;
@@ -33,33 +34,12 @@ export default function MobileMenu({
   setMobShowNewPopup,
   mobtextColor,
 }: MobileMenuProps) {
-  // const [bgColor, setBgColor] = useState<string>("bg-beige");
-  // const [textColor, setTextColor] = useState<string>("text-beige");
-  // const [globalSrc, setGlobalSrc] = useState<any>(glovabIcon);
-  // const [showNewPopup, setShowNewPopup] = useState<boolean>(false);
-
   const mobileMenuRef = useRef<HTMLDivElement | null>(null);
   const showNewPopupRef = useRef<HTMLDivElement | null>(null);
-
-  // const handleScroll = () => {
-  //   if (window.scrollY > 50) {
-  //     setBgColor("bg-secondaryTextColor");
-  //     setGlobalSrc(whiteglovabIcon);
-  //     setTextColor("text-beige");
-  //   } else {
-  //     setBgColor("bg-beige");
-  //     setGlobalSrc(glovabIcon);
-  //     setTextColor("text-black");
-  //   }
-
-  //   setIsOpen(false);
-  //   setShowNewPopup(false);
-  // };
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
 
-    // Close both isOpen and showNewPopup if clicking outside
     const handleClickOutside = (e: MouseEvent) => {
       if (
         mobileMenuRef.current &&
@@ -78,22 +58,25 @@ export default function MobileMenu({
       window.removeEventListener("scroll", handleScroll);
       document.removeEventListener("click", handleClickOutside);
     };
-  }, []);
+  }, [handleScroll]);
 
-  const handleItem2Click = (route: string | undefined) => {
-    if (!route) {
-      setMobShowNewPopup((prev) => !prev);
-    } else {
-      setIsOpen(false);
+  const handleItemClick = (title: string) => {
+    if (title.toLowerCase() !== "Experiences") {
+      setIsOpen(true);
+      setMobShowNewPopup(true);
     }
   };
 
+  const handleItemOnClick = () => {
+    setIsOpen(false);
+    setMobShowNewPopup(false);
+  };
   return (
     <AnimatePresence>
       {isOpen && (
         <motion.div
           ref={mobileMenuRef}
-          className={`fixed  left-0 right-0 z-50 max-1250:block hidden ${mobbgColor} ${mobtextColor} ${mobmarginTop} max-1250:max-w-[1000px] max-350:max-w-[500px] w-full max-h-[calc(100vh-64px)] overflow-y-auto rounded-b-lg`}
+          className={`fixed left-0 right-0 z-50 max-1250:block hidden ${mobbgColor} ${mobtextColor} ${mobmarginTop} max-1250:max-w-[1000px] max-350:max-w-[500px] w-full max-h-[calc(100vh-64px)] overflow-y-auto rounded-b-lg`}
           initial={{ y: -40, opacity: 0 }}
           animate={{ y: 0, opacity: 2 }}
           exit={{ y: -40, opacity: 0 }}
@@ -101,22 +84,23 @@ export default function MobileMenu({
         >
           <div className="w-full shadow-md p-4 max-350:p-2">
             <div className="flex flex-col justify-between h-full">
-              <div className={`flex flex-col gap-5 px-7 py-3  `}>
+              <div className={`flex flex-col gap-5 px-7 py-3`}>
                 {navbar.map((e) =>
                   e.route ? (
                     <Link
                       key={e.id}
                       href={e.route}
-                      className="max-w-[1000px]   relative"
+                      className="max-w-[1000px] relative"
+                      onClick={() => handleItemClick(e.title)}
                     >
-                      <p className="text-xl hover:text-[21px] transition-all ease-in-out duration-300 font-medium max-1250:text-lg max-1150:text-base ">
+                      <p className="text-xl hover:text-[21px] transition-all ease-in-out duration-300 font-medium max-1250:text-lg max-1150:text-base">
                         {e.title}
                       </p>
                     </Link>
                   ) : (
                     <p
                       key={e.id}
-                      onClick={() => handleItem2Click(e.route)}
+                      onClick={() => handleItemClick(e.title)}
                       className="relative"
                     >
                       {e.title}
@@ -126,35 +110,39 @@ export default function MobileMenu({
                 {mobshowNewPopup && (
                   <div
                     ref={showNewPopupRef}
-                    className={`absolute max-w-[300px]  mt-56 ${mobbgColor} ${mobtextColor}  p-4 shadow-md flex flex-col `}
+                    className={`absolute max-w-[300px] bottom-16 left-7 ${mobbgColor} ${mobtextColor} p-4 shadow-md flex flex-col`}
                   >
                     <Link
                       href={"/makrine-restaurant"}
                       className="text-base font-medium my-2 cursor-pointer hover:text-lg transition-all ease-in-out duration-300"
+                      onClick={handleItemOnClick}
                     >
                       Bars & Restaurants
                     </Link>
                     <Link
                       href={"/meetings-events"}
                       className="text-base font-medium my-2 cursor-pointer hover:text-lg transition-all ease-in-out duration-300"
+                      onClick={handleItemOnClick}
                     >
                       Meetings & Events
                     </Link>
                     <Link
                       href={"/wellness-fitness"}
                       className="text-base font-medium my-2 cursor-pointer hover:text-lg transition-all ease-in-out duration-300"
+                      onClick={handleItemOnClick}
                     >
                       Spa & Wellness
                     </Link>
                     <Link
                       href={"/kids-entertainment"}
                       className="text-base font-medium my-2 cursor-pointer hover:text-lg transition-all ease-in-out duration-300"
+                      onClick={handleItemOnClick}
                     >
                       Kids Entertainment
                     </Link>
                   </div>
                 )}
-                <div className="relative w-fit -ml-2  ">
+                <div className="relative w-fit -ml-2 ">
                   <button onClick={() => setPopup((prev) => !prev)}>
                     <Image
                       priority={true}
@@ -167,7 +155,7 @@ export default function MobileMenu({
                   </button>
                   {popup && (
                     <div
-                      className={`absolute top-12 left-0 w-[150px] p-4 shadow-md rounded-md z-50 ${mobbgColor} ${mobtextColor} `}
+                      className={`absolute top-12 left-0 w-[150px] p-4 shadow-md rounded-md z-50 ${mobbgColor} ${mobtextColor}`}
                     >
                       <p className="text-base font-medium my-2 cursor-pointer hover:text-lg transition-all ease-in-out duration-300">
                         English
