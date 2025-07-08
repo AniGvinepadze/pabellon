0f78a4267cc9f84c2d4e08fbf30e1e7f3db822ec
Ani <a.gvin3@gmail.com>
2025-07-08 14:22:28 +0400

    add meeetings

--- app/components/Meetings-events/ConferenceRoomsSection.tsx
+++ app/components/Meetings-events/ConferenceRoomsSection.tsx
@@ -9,41 +9,94 @@ import {
 } from "@/app/assets";
 import Image from "next/image";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { meeting } from "@/app";
 import ContactUsSection from "../Home/ContactUsSection";
 import m1 from "../../../public/assets/meeting1 (1).png";
 import m2 from "../../../public/assets/meeting1 (2).png";
 import m3 from "../../../public/assets/meeting1 (3).png";
 import m4 from "../../../public/assets/meeting1 (4).png";
-
-
+import { iMeeting } from "./ConferenceSection";
+import { axiosInstance } from "@/app/lib/axiosInstance";
+export interface iImageData {
+  imageUrl: string;
+  secondImageUrl: string;
+  thirdImageUrl: string;
+  fourthImageUrl: string;
+}
 export default function ConferenceRoomsSection() {
+  const [data, setData] = useState<iMeeting | null>(null);
+  const [imageData, setImageData] = useState<iImageData | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const language = localStorage.getItem("language") || "en";
+        const response = await axiosInstance.get(
+          `/api/meetings-events?lang=${language}`
+        );
+        const resData = await response.data;
+        if (response.data && response.data.length > 0) {
+          setData(response.data[0]);
+        }
+      } catch (err: any) {
+        setError(err.message ?? "Unknown error");
+      } finally {
+        setLoading(false);
+      }
+    };
+    const fetchDataImg = async () => {
+      try {
+        const language = localStorage.getItem("language") || "en";
+        const response = await axiosInstance.get(
+          `/api/meetings-eventsImg?lang=${language}`
+        );
+        const resData = await response.data;
+        if (response.data && response.data.length > 0) {
+          setImageData(response.data[0]);
+        }
+      } catch (err: any) {
+        setError(err.message ?? "Unknown error");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDataImg();
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div className="text-center py-10">Loading...</div>;
+  }
+  if (error) {
+    return <div className="text-center py-10 text-red-600">Error: {error}</div>;
+  }
+  if (!data) {
+    return <div className="text-center py-10">No data available.</div>;
+  }
+
   return (
     <div className="max-w-[1250px] w-full m-auto flex flex-col justify-center items-center text-center text-secondaryTextColor my-20">
       <div className="max-w-[1000px] w-full flex flex-col justify-center items-center text-center text-secondaryTextColor font-mono-serif gap-6 max-400:gap-3">
         <h2 className="font-semibold text-[40px] italic  max-600:text-[32px] max-400:text-[26px]">
-          Conference Rooms
+          {data.conference_rooms_title}
         </h2>
         <p className="font-light text-[17px] max-600:text-[15px] max-400:text-[13px]">
-          Our fully equipped conference rooms offer a modern, flexible
-          environment for business gatherings. Whether you’re hosting a
-          high-level corporate meeting, a training seminar, or a grand
-          celebration, our hotel offers the ideal setting with multifunctional
-          event spaces designed to meet every need - from professional to
-          personal.
+          {data.conference_rooms_section_description}
         </p>
         <p className="text-[17px] font-light  max-600:text-[15px] max-400:text-[13px]">
-          With a variety of layouts available - from boardroom to classroom or
-          theater-style - our venues can be tailored for meetings, workshops, or
-          private presentations.
+          {data.conference_rooms_section_little_description}
         </p>
       </div>
 
       <div className=" my-20 flex flex-col gap-14 justify-center items-center">
         <Image
           priority={true}
-          src={conferencee}
+          src={`https://pabellona-admin.s3.us-east-1.amazonaws.com/${data.conferenceRoomsImageUrl}`}
           alt="meetingImg"
           width={1420}
           height={877}
@@ -53,61 +106,58 @@ export default function ConferenceRoomsSection() {
       <div className="max-w-[1300px] w-full mb-20 p-7 max-750:p-2 flex flex-col justify-center items-center">
         <div className=" flex gap-10 items-center justify-center text-center m-auto max-w-[950px] w-full max-550:flex-col mb-8 max-550:gap-4">
           <p className="max-w-[250px] w-full font-light text-xl max-800:text-lg max-550:max-w-[300px]">
-            Meeting Room Iliauri - 83.64 m²
+            {data.meeting_room_iliauri}
           </p>
           <p className="max-w-[250px] w-full font-light text-xl max-800:text-lg max-550:max-w-[300px]">
-            Meeting Room Kamara - 83.64 m²
+            {data.meeting_room_kamara}
           </p>
           <p className="font-light max-w-[230px] w-full text-xl max-800:text-lg max-550:max-w-[300px]">
-            Iliauri & Kamara - 167.28 m²
+            {data.kamara_iliauri}
           </p>
         </div>
         <div className=" flex gap-10 items-center text-center justify-center max-w-[650px] my-4 w-full max-550:flex-col max-500:my-0 max-550:gap-4">
           <p className="font-light text-xl max-w-[250px] w-full max-800:text-lg max-550:max-w-[320px]">
-            Boardroom Krebuli Room - 79.92 m²
+            {data.boardroom_krebuli_room}
           </p>{" "}
           <p className="font-light text-xl max-w-[250px] w-full max-800:text-lg max-550:max-w-[300px]">
-            Ballroom Alazani Hall - 355.9 m²
+            {data.ballroom_alazani_hall}
           </p>
         </div>
       </div>
       <div className=" w-full">
-        <div
-         className="w-full grid grid-cols-4 gap-5 max-750:grid-cols-2"
-        >
-            <Image
-              priority={true}
-              src={m1}
-              alt="img"
-              width={500}
-              height={420}
-              className="min-h-[420px] object-cover max-w-[400px] w-full  max-450:min-h-[300px]  "
-            />
-            <Image
-              priority={true}
-              src={m2}
-              alt="img"
-              width={500}
-              height={420}
-              className="min-h-[420px] object-cover max-w-[400px] w-full max-450:min-h-[300px] "
-            />
-            <Image
-              priority={true}
-              src={m3}
-              alt="img"
-              width={500}
-              height={420}
-              className="min-h-[420px] object-cover max-w-[400px] w-full max-450:min-h-[300px] "
-            />
-            <Image
-              priority={true}
-              src={m4}
-              alt="img"
-              width={500}
-              height={420}
-              className="min-h-[420px] object-cover max-w-[400px] w-full max-450:min-h-[300px] "
-            />
-         
+        <div className="w-full grid grid-cols-4 gap-5 max-750:grid-cols-2">
+          <Image
+            priority={true}
+            src={`https://pabellona-admin.s3.us-east-1.amazonaws.com/${imageData?.imageUrl}`}
+            alt="img"
+            width={500}
+            height={420}
+            className="min-h-[420px] object-cover max-w-[400px] w-full  max-450:min-h-[300px]  "
+          />
+          <Image
+            priority={true}
+            src={`https://pabellona-admin.s3.us-east-1.amazonaws.com/${imageData?.secondImageUrl}`}
+            alt="img"
+            width={500}
+            height={420}
+            className="min-h-[420px] object-cover max-w-[400px] w-full max-450:min-h-[300px] "
+          />
+          <Image
+            priority={true}
+            src={`https://pabellona-admin.s3.us-east-1.amazonaws.com/${imageData?.thirdImageUrl}`}
+            alt="img"
+            width={500}
+            height={420}
+            className="min-h-[420px] object-cover max-w-[400px] w-full max-450:min-h-[300px] "
+          />
+          <Image
+            priority={true}
+            src={`https://pabellona-admin.s3.us-east-1.amazonaws.com/${imageData?.fourthImageUrl}`}
+            alt="img"
+            width={500}
+            height={420}
+            className="min-h-[420px] object-cover max-w-[400px] w-full max-450:min-h-[300px] "
+          />
         </div>
       </div>
       <div></div>
--- app/components/Meetings-events/ConferenceSection.tsx
+++ app/components/Meetings-events/ConferenceSection.tsx
@@ -9,10 +9,18 @@ export interface iMeeting {
   conference_ballroom_section_little_description: string;
   conference_ballroom_section_description: string;
   spaces_title: string;
+  spaces_meeting_room: string;
+  spaces_boardroom: string;
+  spaces_ballroom: string;
   conference_rooms_title: string;
   conference_rooms_section_description: string;
   conference_rooms_section_little_description: string;
+  meeting_room_iliauri: string;
+  meeting_room_kamara: string;
+  kamara_iliauri: string;
 
+  boardroom_krebuli_room: string;
+  ballroom_alazani_hall: string;
   conferenceImageUrl: string;
   artWorkImageUrl: string;
   conferenceRoomsImageUrl: string;
@@ -32,7 +40,7 @@ export default function ConferenceSection() {
           `/api/meetings-events?lang=${language}`
         );
         const resData = await response.data;
-         if (response.data && response.data.length > 0) {
+        if (response.data && response.data.length > 0) {
           setData(response.data[0]);
         }
       } catch (err: any) {
@@ -44,8 +52,6 @@ export default function ConferenceSection() {
 
     fetchData();
   }, []);
-
-
 
   if (loading) {
     return <div className="text-center py-10">Loading...</div>;
@@ -61,19 +67,19 @@ export default function ConferenceSection() {
     <div className="max-w-[1250px] w-full m-auto flex flex-col justify-center items-center text-center text-secondaryTextColor mt-20">
       <div className="max-w-[1250px] w-full flex flex-col justify-center items-center text-center text-secondaryTextColor font-mono-serif gap-6 max-400:gap-3">
         <h2 className="font-semibold text-[40px] italic  max-600:text-[32px] max-400:text-[26px]">
-        {data.conference_ballroom_title}
+          {data.conference_ballroom_title}
         </h2>
         <h2 className="font-normal text-[30px] italic max-600:text-[22px] max-400:text-[18px] custom-font">
-         {data.conference_ballroom_section_little_description}
+          {data.conference_ballroom_section_little_description}
         </h2>
         <p className="text-[17px] font-light max-w-[1030px] max-600:text-[15px] max-400:text-[13px]">
-         {data.conference_ballroom_section_description}
+          {data.conference_ballroom_section_description}
         </p>
       </div>
       <div className="my-20">
         <Image
           priority={true}
-           src={`https://pabellona-admin.s3.us-east-1.amazonaws.com/${data.conferenceImageUrl}`}
+          src={`https://pabellona-admin.s3.us-east-1.amazonaws.com/${data.conferenceImageUrl}`}
           alt="icon"
           width={1540}
           height={940}
