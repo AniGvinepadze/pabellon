81071f11a4c169af35605ede948c7c64fa95be7e
Ani <a.gvin3@gmail.com>
2025-07-19 23:11:02 +0400

    Fix ( Makrine-menu ) : fixed menuLogo

--- app/components/Makrine-Resraurant/MakrineMenu.tsx
+++ app/components/Makrine-Resraurant/MakrineMenu.tsx
@@ -6,7 +6,46 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function MakrineMenu() {
-  
+  const [restaurantData, setRestaurantData] = useState<IMakrine | null>(null);
+
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const language = "en";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const language = localStorage.getItem("language") || "en";
+
+      try {
+        const response = await axiosInstance.get(
+          `/api/restaurantbar?lang=${language}`
+        );
+        setRestaurantData(response.data);
+
+        if (response.data && response.data.length > 0) {
+          setRestaurantData(response.data[0]);
+        }
+      } catch (err: any) {
+        setError(err.message ?? "Unknown error");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!restaurantData) {
+    return <div>No data available.</div>;
+  }
   return (
     <div className="max-w-[1250px] w-full m-auto flex flex-col justify-center items-center text-center text-secondaryTextColor my-20">
       <div className="max-w-[500px] w-full flex flex-col justify-center items-center text-center text-secondaryTextColor font-mono-serif gap-12 max-400:gap-3">
@@ -26,6 +65,24 @@ export default function MakrineMenu() {
         </Link>
       </div>
 
+      {/* <div className="max-w-[1200px] w-full my-20 flex gap-10 justify-between items-center max-1250:flex-col p-3">
+        <Image
+          priority={true}
+          src={`https://pabellona-admin.s3.us-east-1.amazonaws.com/${restaurantData.menuImageUrl}`}
+          alt="menuImageUrl"
+          width={620}
+          height={677}
+          className="max-w-[600px] w-full rounded-xl object-contain"
+        />
+        <Image
+          priority={true}
+          src={`https://pabellona-admin.s3.us-east-1.amazonaws.com/${restaurantData.menuImageUrlSecondPart}`}
+          alt="menuImageUrl"
+          width={620}
+          height={877}
+          className="max-w-[600px] w-full rounded-xl object-contain"
+        />
+      </div> */}
     </div>
   );
 }
