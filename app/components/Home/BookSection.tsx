d9bf9464aa92e64c0ab46fe1ce53eb0b14491bf5
Ani <a.gvin3@gmail.com>
2025-07-22 14:35:44 +0400

    Feat ( Services ) : connected book section to adminJs

--- app/components/Home/BookSection.tsx
+++ app/components/Home/BookSection.tsx
@@ -1,8 +1,15 @@
 "use client";
+import { axiosInstance } from "@/app/lib/axiosInstance";
 import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+type BooHomekData = {
+  book_title: string;
+  check_in: string;
+  check_out: string;
+  bookBtn: string;
+};
 
 export default function BookSection() {
   const [checkIn, setCheckIn] = useState<Date | null>(null);
@@ -11,8 +18,27 @@ export default function BookSection() {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [selectedGuest, setSelectedGuest] = useState<number | null>(null);
   const popupRef = useRef<HTMLDivElement>(null);
-
+  const [data, setData] = useState<BooHomekData | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const language = localStorage.getItem("language") || "en";
+        const response = await axiosInstance.get(
+          `/api/bookHome?lang=${language}`
+        );
+        const resData = await response.data;
+        console.log(resData);
+        setData(resData);
+      } catch (err: any) {
+        setError(err.message ?? "Unknown error");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
     const handleClickOutside = (event: MouseEvent) => {
       if (
         popupRef.current &&
@@ -36,11 +62,21 @@ export default function BookSection() {
     setSelectedGuest(value);
     setIsPopupOpen(false);
   };
+  if (loading) {
+    return <div className="text-center py-10">Loading...</div>;
+  }
+  if (error) {
+    return <div className="text-center py-10 text-red-600">Error: {error}</div>;
+  }
+  if (!data) {
+    return <div className="text-center py-10">No data available.</div>;
+  }
+
   return (
     <div className=" max-w-[1600px] w-full m-auto my-7 p-5">
       <div className=" max-w-[1600px] w-full flex flex-col justify-center items-center text-center">
-        <h2 className="text-[40px] font-normal my-10 max-500:my-4 max-550:text-[34px] max-450:text-[30px] max-400:text-[25px] max-350:text-[21px] max-300:text-[18px] max-300:my-1">
-          Book Your Stay At Pabellón
+        <h2 className="text-[32px] font-normal my-10 max-500:my-4 max-550:text-[28px] max-450:text-[24px]  max-350:text-[21px] max-300:text-[18px] max-300:my-1">
+          {data.book_title}
         </h2>
         <div className="grid grid-cols-4 gap-10 items-center relative max-w-[800px] m-auto w-full justify-between mt-6 max-600:grid-cols-2 max-600:px-10 max-450:px-3 max-450:gap-6 max-400:px-0">
           <div className="flex flex-col w-full cursor-pointer hover:scale-105 transition-all ease-in-out duration-300 ">
@@ -50,7 +86,7 @@ export default function BookSection() {
               selectsStart
               startDate={checkIn}
               endDate={checkOut}
-              placeholderText="Check-In"
+              placeholderText={data.check_in}
               className="border-none text-lg bg-beige placeholder-black outline-none font-serif max-400:text-base custom-date-selector"
             />
             <div className="w-full h-[1px]  bg-black my-3" />
@@ -63,7 +99,7 @@ export default function BookSection() {
               selectsEnd
               startDate={checkIn}
               endDate={checkOut}
-              placeholderText="Check-Out"
+              placeholderText={data.check_out}
               className="border-none bg-beige  placeholder-black  outline-none font-serif w-[150px] max-400:text-base"
             />
             <div className="w-full h-[1px] bg-black my-3" />
@@ -98,7 +134,7 @@ export default function BookSection() {
             className="max-600:col-span-2"
           >
             <button className="max-w-[140px] w-full text-lg font-medium cursor-pointer hover:scale-110  transition-all ease-in-out duration-300 max-400:text-base">
-              Book
+              {data.bookBtn}
             </button>
             <div className="hidden w-full h-[1px] bg-black my-3 max-600:flex" />
           </Link>
--- app/components/Services/RestaurantBookSection.tsx
+++ app/components/Services/RestaurantBookSection.tsx
@@ -1,7 +1,15 @@
 "use client";
+import { axiosInstance } from "@/app/lib/axiosInstance";
 import React, { useEffect, useRef, useState } from "react";
 import DatePicker from "react-datepicker";
-
+type BookResData = {
+  book_title: string;
+  check_in: string;
+  check_out: string;
+  bookBtn: string;
+  select_date: string;
+  adults: string;
+};
 export default function RestaurantBookSection() {
   const [checkIn, setCheckIn] = useState<Date | null>(null);
   const [checkOut, setCheckOut] = useState<Date | null>(null);
@@ -9,8 +17,27 @@ export default function RestaurantBookSection() {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [selectedGuest, setSelectedGuest] = useState<number | null>(null);
   const popupRef = useRef<HTMLDivElement>(null);
-
+  const [data, setData] = useState<BookResData | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const language = localStorage.getItem("language") || "en";
+        const response = await axiosInstance.get(
+          `/api/bookServices?lang=${language}`
+        );
+        const resData = await response.data;
+        console.log(resData);
+        setData(resData);
+      } catch (err: any) {
+        setError(err.message ?? "Unknown error");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
     const handleClickOutside = (event: MouseEvent) => {
       if (
         popupRef.current &&
@@ -34,19 +61,29 @@ export default function RestaurantBookSection() {
     setSelectedGuest(value);
     setIsPopupOpen(false);
   };
+  if (loading) {
+    return <div className="text-center py-10">Loading...</div>;
+  }
+  if (error) {
+    return <div className="text-center py-10 text-red-600">Error: {error}</div>;
+  }
+  if (!data) {
+    return <div className="text-center py-10">No data available.</div>;
+  }
+
   return (
     <div className="w-full m-auto text-secondaryTextColor my-40 max-700:my-0">
       <div className="max-w-[1250px] w-full m-auto flex flex-col justify-center">
         <div className="flex justify-center my-10 max-500:my-0">
           <h2 className="italic font-semibold text-[40px] max-500:text-[32px]">
-            Book Restaurant
+            {data.book_title}
           </h2>
         </div>
         <div className="flex gap-10 items-center relative max-w-[1000px] m-auto w-full justify-between mt-6  max-600:px-10 max-450:px-3 max-450:gap-6 max-400:px-0 max-650:flex-col">
           <div className="w-full flex justify-between gap-7 ">
             <div className="flex flex-col w-full cursor-pointer transition-all ease-in-out duration-300 ">
               <p className="font-normal text-[17px] my-8 max-500:text-[15px]">
-                Select Date
+                {data.select_date}
               </p>
               <DatePicker
                 selected={checkIn}
@@ -54,7 +91,7 @@ export default function RestaurantBookSection() {
                 selectsStart
                 startDate={checkIn}
                 endDate={checkOut}
-                placeholderText="July 6, 2025"
+                placeholderText={data.check_in}
                 className="border-none max-w-[400px] w-full text-lg bg-beige placeholder-black outline-none font-serif max-400:text-base cursor-pointer"
               />
               <div className="w-full h-[1px]  bg-black my-3" />
@@ -62,7 +99,7 @@ export default function RestaurantBookSection() {
 
             <div className="flex flex-col w-full  cursor-pointer  transition-all ease-in-out duration-300 max-600:col-span-2 max-400:-mt-1">
               <p className="font-normal text-[17px] my-8 max-500:text-[15px] ">
-                Adults
+                {data.adults}
               </p>
               <input
                 type="text"
@@ -91,7 +128,7 @@ export default function RestaurantBookSection() {
           </div>
           <div className="max-w-[150px] w-full max-650:max-w-[650px] mt-16">
             <button className=" w-full px-2 py-2  text-base font-medium cursor-pointer hover:scale-110  transition-all ease-in-out duration-300 max-400:text-base text-beige  bg-secondaryTextColor">
-              make an enquiry
+              {data.bookBtn}
             </button>
           </div>
         </div>
