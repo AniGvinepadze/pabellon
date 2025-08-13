cf6336937d093971df6febefe833523fb51b1f16
Ani <a.gvin3@gmail.com>
2025-08-13 16:47:23 +0400

    change base url

--- app/lib/axiosInstance.ts
+++ app/lib/axiosInstance.ts
@@ -2,5 +2,10 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: "https://fresh-adminjs-ilqo.onrender.com",
+
+
+
+
+  
   // baseURL: "http://localhost:3001",
 });
